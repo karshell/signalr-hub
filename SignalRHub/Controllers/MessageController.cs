b1642ce4b92aca3ef5896515c0dfcf33892f6046
Kevin Shelley <kevin.shelley@sonova.com>
2018-10-25 17:55:56 -0400

    initial commit, basic signalling working

---
+++ SignalRHub/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRHub.Controllers
+{
+    public class MessageController : ControllerBase
+    {
+        private IHubContext<NotifyHub, ITypedHubClient> _hubContext;
+
+        public MessageController(IHubContext<NotifyHub, ITypedHubClient> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost("[controller]/send/{groupId}")]
+        public async Task<IActionResult> Post(string groupId, [FromBody]Message msg)
+        {
+           try
+            {
+                await _hubContext.Clients.Group(groupId).BroadcastMessage(msg.Type, msg.Payload);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ SignalRHub/NotifyHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRHub
+{
+    public class NotifyHub : Hub<ITypedHubClient>
+    {
+        public void JoinGroup(string groupName)
+        {
+            Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+    }
+}
